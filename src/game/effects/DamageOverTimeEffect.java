cdd40474dc05b88eb4c249d02066c08351e57b54
AungKhant169 <aungkhantaz@gmail.com>
2026-04-13 14:26:36 +0800

    minor change

--- src/game/effects/DamageOverTimeEffect.java
+++ src/game/effects/DamageOverTimeEffect.java
@@ -4,16 +4,16 @@ import game.entities.Combatant;
 import game.ui.UI;
 
 public class DamageOverTimeEffect extends Debuff {
-	private int DOTdamage;
-	public DamageOverTimeEffect(String Name, int cooldown, int DOTdamage) {
-		super(Name, cooldown);
-		this.DOTdamage = DOTdamage;
+	private int dotDamage;
+	public DamageOverTimeEffect(String name, int duration, int dotDamage) {
+		super(name, duration);
+		this.dotDamage = dotDamage;
 	}
 
 	public void inflectDamage(Combatant c, UI ui) {
 //		String statusDamageMessage = c.getName() + " takes poison damage -> HP: " + c.getCurrentHp() + " -> ";
 		String statusDamageMessage = "HP: From " + c.getCurrentHp() + " -> To ";
-		c.takeDamage(this.DOTdamage);
+		c.takeDamage(this.dotDamage);
 		statusDamageMessage = statusDamageMessage + c.getCurrentHp()+" ";
 //		ui.displayActionResult(statusDamageMessage);
         ui.displayActionResult("takes "+this.getName()+" damage", c, null, "", statusDamageMessage);
